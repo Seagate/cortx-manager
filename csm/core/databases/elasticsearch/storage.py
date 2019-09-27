f043d7840ec3428622a519a7b092ef5c1375c206
Alexander Nogikh <alexander.nogikh@seagate.com>
2019-09-27 14:58:47 +0000

    Minor code fixes

--- csm/core/databases/elasticsearch/storage.py
+++ csm/core/databases/elasticsearch/storage.py
@@ -2,12 +2,12 @@ from elasticsearch_dsl import Q
 from schematics.models import FieldDescriptor
 from csm.core.blogic.filters import IFilterTreeVisitor, \
     FilterOperationAnd, FilterOperationOr, FilterOperationCompare
-from csm.core.blogic.filters import ComparisonOperation
+from csm.core.blogic.filters import ComparisonOperation, IFilterQuery
 
 
 def _match_query(field: str, target):
     obj = {
-        field: value
+        field: target
     }
 
     return Q("match", **obj)
@@ -16,7 +16,7 @@ def _match_query(field: str, target):
 def _range_generator(op_string: str):
     def _make_query(field: str, target):
         obj = {
-            left_op: {
+            field: {
                 op_string: target
             }
         }
@@ -53,31 +53,31 @@ class ElasticSearchQueryConverter(IFilterTreeVisitor):
         operands = entry.get_operands()
         if len(operands) < 2:
             raise Exception("Malformed AND operation: fewer than two arguments")
-        
+
         ret = operands[0].accept_visitor(self)
         for operand in operands[1:]:
             ret = ret & operand.accept_visitor(operand)
-        
+
         return ret
 
     def handle_or(self, entry: FilterOperationOr):
         operands = entry.get_operands()
         if len(operands) < 2:
             raise Exception("Malformed OR operation: fewer than two arguments")
-        
+
         ret = operands[0].accept_visitor(self)
         for operand in operands[1:]:
             ret = ret | operand.accept_visitor(operand)
-        
+
         return ret
 
     def handle_compare(self, entry: FilterOperationCompare):
         field = entry.get_left_operand()
         field_str = None
 
-        if instanceof(field, str):
+        if isinstance(field, str):
             field_str = field
-        elif instanceof(field, FieldDescriptor):
+        elif isinstance(field, FieldDescriptor):
             field_str = field.name
         else:
             raise Exception("Failed to process left side of comparison operation")
