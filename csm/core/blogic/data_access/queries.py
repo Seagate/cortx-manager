6b0e912a4ad95cd2ec79e8e8b8a82d99de1f879a
Dmitry Didenko <dimitry.didenko@seagate.com>
2019-10-06 18:58:11 +0000

    EOS-1763: Add delete/count functionality for storage. Fix asyncronous work with db provider, db drivers and storages. Others minor fixes

--- csm/core/blogic/data_access/queries.py
+++ csm/core/blogic/data_access/queries.py
@@ -1,17 +1,19 @@
 from schematics.types import BaseType
 
 from csm.core.blogic.data_access.filters import IFilterQuery
+from enum import Enum
 
 
-ASC = "asc"  # Ascending
-DESC = "desc"  # Descending
+class SortOrder(Enum):
+    ASC = "asc"
+    DESC = "desc"
 
 
 class OrderBy:
 
     """Class to represent order by parameters for DB"""
 
-    def __init__(self, field, order: int = ASC):
+    def __init__(self, field, order: int = SortOrder.ASC):
         self.field = field
         self.order = order
 
@@ -24,21 +26,22 @@ class Query:
 
         """Data storage class for Query parameters"""
 
-        def __init__(self, order_by: OrderBy = None, group_by: BaseType = None,
-                     filter_by: IFilterQuery = None, limit: int = None, offset: int = None):
+        # TODO: it can be a schematics model
+        def __init__(self, order_by: OrderBy = None, filter_by: IFilterQuery = None, limit: int = None,
+                     offset: int = None):
 
             self.order_by = order_by
-            self.group_by = group_by
             self.filter_by = filter_by
             self.limit = limit
             self.offset = offset
 
-    def __init__(self, order_by: OrderBy = None, group_by: BaseType = None,
-                 filter_by: IFilterQuery = None, limit: int = None, offset: int = None):
+    def __init__(self, order_by: OrderBy = None, filter_by: IFilterQuery = None, limit: int = None,
+                 offset: int = None):
 
-        self.data = self.Data(order_by, group_by, filter_by, limit, offset)
+        self.data = self.Data(order_by, filter_by, limit, offset)
 
-    def order_by(self, by_field: BaseType, by_order: int = ASC):
+    # TODO: order_by can be chained and we can store an array of fields to sort by them
+    def order_by(self, by_field: BaseType, by_order: int = SortOrder.ASC):
         """
         Set Query order_by parameter
 
@@ -47,15 +50,6 @@ class Query:
 
         """
         self.data.order_by = OrderBy(by_field, by_order)
-
-    def group_by(self, by_field: BaseType):
-        """
-        Set Query group_by parameter
-
-        :param BaseType by_field: field for grouping
-
-        """
-        self.data.group_by = by_field
         return self
 
     def filter_by(self, by_filter: IFilterQuery):
@@ -96,5 +90,29 @@ class ExtQuery(Query):
 
     """Storage Extended Query API used by Storage aggregation functions"""
 
+    class Data:
+
+        """Data storage class for Query parameters"""
+
+        # TODO: it can be a schematics model
+        def __init__(self, order_by: OrderBy = None, group_by: BaseType = None,
+                     filter_by: IFilterQuery = None, limit: int = None, offset: int = None):
+
+            self.order_by = order_by
+            self.group_by = group_by
+            self.filter_by = filter_by
+            self.limit = limit
+            self.offset = offset
+
     def __init__(self):
         super().__init__()
+
+    def group_by(self, by_field: BaseType):
+        """
+        Set Query group_by parameter
+
+        :param BaseType by_field: field for grouping
+
+        """
+        self.data.group_by = by_field
+        return self
--- csm/core/databases/generic_storage.py
+++ csm/core/databases/generic_storage.py
@@ -1,6 +1,7 @@
 
 from csm.core.blogic.data_access import IStorage
 from csm.core.blogic.data_access import ExtQuery
+from csm.core.blogic.data_access import IFilterQuery
 
 
 class BaseAbstractStorage(IStorage):
@@ -8,42 +9,54 @@ class BaseAbstractStorage(IStorage):
     """Base Abstract Storage class for aggregation functions"""
 
     async def sum(self, ext_query: ExtQuery):
-        """Sum Aggregation function
+        """
+        Sum Aggregation function
 
-            :param ExtQuery ext_query: Extended query which describes how to perform sum aggregation
-
+        :param ExtQuery ext_query: Extended query which describes how to perform sum aggregation
+        :return:
         """
         pass
 
     async def avg(self, ext_query: ExtQuery):
         """Average Aggregation function
 
-            :param ExtQuery ext_query: Extended query which describes how to perform average
-                                       aggregation
-
+        :param ExtQuery ext_query: Extended query which describes how to perform average
+                                   aggregation
+        :return:
         """
         pass
 
+    async def simple_count(self, filter_obj: IFilterQuery) -> int:
+        """
+        Returns count of entities for given filter_obj
+
+        :param filter_obj:
+        :return:
+        """
+
     async def count(self, ext_query: ExtQuery):
-        """Count Aggregation function
+        """
+        Count Aggregation function
 
-            :param ExtQuery ext_query: Extended query which describes to perform count aggregation
-
+        :param ExtQuery ext_query: Extended query which describes to perform count aggregation
+        :return:
         """
         pass
 
     async def max(self, ext_query: ExtQuery):
-        """Max Aggregation function
+        """
+        Max Aggregation function
 
-            :param ExtQuery ext_query: Extended query which describes how to perform Max aggregation
-
+        :param ExtQuery ext_query: Extended query which describes how to perform Max aggregation
+        :return:
         """
         pass
 
     async def min(self, ext_query: ExtQuery):
-        """Min Aggregation function
+        """
+        Min Aggregation function
 
-            :param ExtQuery ext_query: Extended query which describes how to perform Min aggregation
-
+        :param ExtQuery ext_query: Extended query which describes how to perform Min aggregation
+        :return:
         """
         pass
