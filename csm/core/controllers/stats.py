e041d1ae4c208c16604bdcb8a9fe7118f9c1bcab
Eduard Aleksandrov <eduard.aleksandrov@seagate.com>
2019-11-06 10:38:23 +0000

    EOS-1630: CSM Stats and controller - fixed default values for metric_list and query

--- csm/core/controllers/stats.py
+++ csm/core/controllers/stats.py
@@ -38,10 +38,10 @@ class StatsView(CsmView):
         panel = self.request.match_info["panel"]
         from_t = self.request.rel_url.query.get("from", None)
         to_t = self.request.rel_url.query.get("to", None)
-        metric_list = self.request.rel_url.query.getall("metric_list", None)
+        metric_list = self.request.rel_url.query.getall("metric_list", [])
         interval = self.request.rel_url.query.get("interval", None)
         output_format = self.request.rel_url.query.get("output_format", "gui")
-        query = self.request.rel_url.query.get("query", None)
+        query = self.request.rel_url.query.get("query", "")
 
         return await self._service.get(stats_id, panel, from_t, to_t, metric_list,
                                         interval, output_format, query)
