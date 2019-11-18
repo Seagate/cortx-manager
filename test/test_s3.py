56effd3648084510eb27250a400b01879151ebca
Alexander Voronov <alexander.voronov@seagate.com>
2019-11-18 16:25:28 +0000

    EOS-2862: implement the initial version of buckets cache
    Introduces the S3BucketsCache class that represents self-sustaining
    cache of buckets for particular S3 account.

--- test/test_s3.py
+++ test/test_s3.py
@@ -32,10 +32,10 @@ if __name__ == '__main__':
         if not any(x.account_name == account.account_name for x in account_list.iam_accounts):
             print("There is no account in the account list")
             return
-        
+
         print("Test account has been created successfully")
         return account
-    
+
     async def _test_create_list_delete_bucket(account: ExtendedIamAccount):
         s3cli = pl.get_s3_client(account.access_key_id, account.secret_key_id, s3_conf)
         bucket_name = 's3plugintest'
@@ -44,7 +44,7 @@ if __name__ == '__main__':
         if bucket is None:
             print("Cannot create bucket ", bucket_name)
             return
-        
+
         bucket_list = await s3cli.get_all_buckets()
         if bucket_list is None:
             print("Cannot get the bucket list")
@@ -53,7 +53,7 @@ if __name__ == '__main__':
         if not any(b.name == bucket_name for b in bucket_list):
             print("The bucket has not been created")
             return
-        
+
         await s3cli.delete_bucket(bucket_name)
 
         bucket_list = await s3cli.get_all_buckets()
@@ -64,17 +64,36 @@ if __name__ == '__main__':
         if any(b.name == bucket_name for b in bucket_list):
             print("The bucket has not been deleted")
             return
-        
+
+        print("Bucket create/list/delete done successfully")
+
+    async def _test_s3_buckets_cache(account: ExtendedIamAccount):
+        cache = pl.get_s3_buckets_cache(account.access_key_id, account.secret_key_id, s3_conf, 2)
+        #S3BucketsCache(account.access_key_id, account.secret_key_id, s3_conf, 2, loop)
+        s3cli = pl.get_s3_client(account.access_key_id, account.secret_key_id, s3_conf)
+
+        test_bucket_name = 'tests3cachebucket'
+        await s3cli.create_bucket(test_bucket_name)
+        await asyncio.sleep(3)
+        buckets = cache.get_cache()
+
+        if not any(b.name == test_bucket_name for b in buckets):
+            print("The bucket is not found in cache")
+            return
+
+        print("Bucket cache verified successfully")
+        await s3cli.delete_bucket(test_bucket_name)
+
 
     async def _test_delete_account(account: ExtendedIamAccount):
         delete_client = pl.get_iam_client(account.access_key_id, account.secret_key_id, iam_conf)
         result = await delete_client.delete_account(account.account_name)
 
-        if isinstance(result, bool) and result:
-            print("Test is completed!")
-        else:
+        if not (isinstance(result, bool) and result):
             print("Cannot delete the account")
+        print("Test account has been deleted successfully")
 
     account = loop.run_until_complete(_test_create_account())
     loop.run_until_complete(_test_create_list_delete_bucket(account))
+    loop.run_until_complete(_test_s3_buckets_cache(account))
     loop.run_until_complete(_test_delete_account(account))
