70bee3d1251be38634b57d9b45083f35728e11be
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2019-12-03 18:16:48 +0000

    Unit conversion in capacity gauge api

--- csm/core/services/storage_capacity.py
+++ csm/core/services/storage_capacity.py
@@ -23,6 +23,7 @@ import subprocess
 
 from csm.common.log import Log
 from csm.common.services import ApplicationService
+from csm.core.blogic import const
 from csm.core.providers.providers import Response
 
 
@@ -31,31 +32,45 @@ class StorageCapacityService(ApplicationService):
     Service for Get disk capacity details
     """
 
+    @staticmethod
+    async def unit_conversion(capacity):
+        """
+        Method to dynamically convert byte data in KB/MB/GB ... YB.
+
+        :param capacity: Disk size in bytes :type: int
+        :return: :type: str
+        """
+        for unit in const.UNIT_LIST:
+            capacity = capacity / 1024
+            if capacity / 100 < 10:
+                break
+
+        return f'{round(capacity, 2)} {unit}'
+
     @Log.trace_method(Log.DEBUG)
     async def get_capacity_details(self):
         """
         This method will return system disk details as per command
         :return: dict
         """
-        # TODO: 'hctl mero status' command will be changed when HALON is replaced with HARE
-        process = subprocess.Popen(['hctl','mero','status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
 
+        # TODO: 'hctl mero status' command will be changed when HALON is replaced with HARE
+        process = subprocess.Popen(const.HCTL_COMMAND, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
         if not stdout:
             Log.error(stderr)
             return Response(rc=422, output=f"{stderr}")
+        console_output = (re.sub(' +', ' ', stdout.decode('utf-8')).split('\n'))
+        cropped_console_data = [str(each_element).strip() for each_element in console_output if
+                                "space" in str(each_element)]
+        capacity_info = {key: int(value.replace(',', '')) for key, value in
+                         (data.split(':') for data in cropped_console_data)}
+        formatted_output = {}
+        formatted_output['size'] = await self.unit_conversion(capacity_info['Total space'])
+        formatted_output['used'] = await self.unit_conversion(
+            capacity_info['Total space'] - capacity_info['Free space'])
+        formatted_output['avail'] = await self.unit_conversion(capacity_info['Free space'])
+        formatted_output['usage_percentage'] = str(
+            100 - round((capacity_info['Free space'] / capacity_info['Total space']) * 100, 2)) + ' %'
 
-        data_list = [str(space).strip() for space in (re.sub(' +', ' ', stdout.decode('utf-8')).split('\n')) if ("space") in str(space)]
-
-        data_dict = {key:int(val.replace(',','')) for key,val in (data.split(':') for data in data_list) }
-
-        formatted_output_dict = {}
-        formatted_output_dict['size'] = str(round(data_dict['Total space'] / (1024**3),2)) + ' GB'
-        formatted_output_dict['used'] = str(round((data_dict['Total space'] - data_dict['Free space']) / (1024**3),2)) + ' GB'
-        formatted_output_dict['avail'] = str(round(data_dict['Free space'] / (1024**3),2)) + ' GB'
-        formatted_output_dict['usage_percentage'] =  str(100- round( (data_dict['Free space'] / data_dict['Total space']) * 100 ,2)) + ' %'
-
-
-        return  formatted_output_dict
-
-
+        return formatted_output
