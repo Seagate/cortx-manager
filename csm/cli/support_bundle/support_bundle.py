de9551be83621cfa60541051526a3278a6c63b73
Prathamesh Rodi <prathamesh.rodi@seagate.com>
2020-05-14 06:09:44 +0000

    EOS-7115   Changes for supporting passwordless ssh with CSM User.

--- csm/cli/support_bundle/support_bundle.py
+++ csm/cli/support_bundle/support_bundle.py
@@ -24,6 +24,7 @@ import random
 import asyncio
 import shutil
 import errno
+import getpass
 from threading import Thread
 from csm.common.payload import Yaml, JsonMessage
 from csm.core.blogic import const
@@ -46,7 +47,7 @@ class SupportBundle:
     """
     @staticmethod
     def execute_ssh(ip_address: str, bundle_id: str, comment: str,
-                    node_name: str):
+                    node_name: str, current_user: str, ssh_key: str):
         """
         This Method makes Connection on each node and Executes Bundle Generation Command.
         For Using this Method Need to setup ssh keys in ~/.ssh/ folder.
@@ -62,11 +63,12 @@ class SupportBundle:
         :param bundle_id: Unique ID of the Bundle needed to be generated. :type: str
         :param comment: Reason to generate the SB. :type: str
         :param node_name: Name of the NODE in EOS Term :type: str
+        :param current_user : Local User Name for SSH :type str
+        :param ssh_key: SSH Key Path :type str.
         :return:
         """
         try:
-            ssh_key = os.path.expanduser(os.path.join("~", ".ssh", const.SSH_KEY))
-            ssh_conn_object = SSHChannel(ip_address, user=const.SSH_USER_NAME,
+            ssh_conn_object = SSHChannel(ip_address, user=current_user,
                                          key_filename=ssh_key)
             ssh_conn_object.connect()
             bundle_generate = (
@@ -76,6 +78,7 @@ class SupportBundle:
                 # Executes Shell Script to Run in Background.
                 # Time out releases the connection rather than waiting for cmd.
                 # Since Shell Runs in Background no response is expected from Shell Command.
+                Log.debug(f"Executing Command {bundle_generate}")
                 ssh_conn_object.execute(bundle_generate, timeout=2)
             except CsmError:
                 Log.debug(f"Started Bundle Generation on {ip_address}")
@@ -143,6 +146,10 @@ class SupportBundle:
         :param command: Csm_cli Command Object :type: command
         :return: None
         """
+        current_user = str(getpass.getuser())
+        if current_user.lower() != "root":
+            repsonse_msg = "Support Bundle Command requires root privileges."
+            return Response(rc=errno.EACCES, output=repsonse_msg),
         alphabet = string.ascii_lowercase + string.digits
         bundle_id = f"SB{''.join(random.choices(alphabet, k=8))}"
         comment = command.options.get("comment")
@@ -153,10 +160,14 @@ class SupportBundle:
         if not isinstance(hostnames, list):
             return hostnames
         # Start Daemon Threads for Starting SB Generation on all Nodes.
+
+        ssh_key = os.path.expanduser(os.path.join("~", ".ssh", const.SSH_KEY))
+        Log.debug(f"Current User > {current_user}, ssh key > {ssh_key}")
         for index, hostname in enumerate(hostnames):
             Log.debug(f"Connect to {hostname}")
             thread_obj = Thread(SupportBundle.execute_ssh(hostname,
-                            bundle_id, comment, node_list[index]), daemon=True)
+                            bundle_id, comment, node_list[index], current_user,
+                                                      ssh_key), daemon=True)
             thread_obj.start()
         symlink_path = Conf.get(const.CSM_GLOBAL_INDEX,
                                 "SUPPORT_BUNDLE.symlink_path")
