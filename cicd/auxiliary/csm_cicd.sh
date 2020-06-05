6f914166301ee492a083913eac5d1fc7002cf409
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2020-06-05 11:31:50 +0000

    Update csm_cicd.sh

--- cicd/auxiliary/csm_cicd.sh
+++ cicd/auxiliary/csm_cicd.sh
@@ -20,6 +20,8 @@ mkdir -p /opt/seagate/eos-prvsnr/generated_configs/healthmap/
 cp -f $CSM_REPO_PATH/jenkins/cicd/etc/ees-schema.json /opt/seagate/eos-prvsnr/generated_configs/healthmap/
 chmod 777 /opt/seagate/eos-prvsnr/generated_configs/healthmap/ees-schema.json
 
+salt-call grains.get id --out=json
+salt-call grains.get roles --out=json
 
 csm_setup post_install
 csm_setup config --debug
