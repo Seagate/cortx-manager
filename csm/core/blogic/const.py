83636d4b305a4f7ea6fc3829368ef38bbdaa5a4f
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2020-06-11 03:49:55 -0600

    Changing product name to cortx

--- csm/core/blogic/const.py
+++ csm/core/blogic/const.py
@@ -11,6 +11,8 @@ CSM_CONF_PATH = ETC_PATH + "/csm"
 CSM_SOURCE_CONF = "{}/conf/etc/csm/csm.conf".format(CSM_PATH)
 CSM_SETUP_LOG_DIR = "/tmp"
 CSM_CONF_FILE_NAME = 'csm.conf'
+PLUGIN_DIR = 'eos'
+WEB_DIR = 'eos'
 
 # Access log of aiohttp
 # format
