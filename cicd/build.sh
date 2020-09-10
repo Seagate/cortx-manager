8bf635638379924a016596a74ad4441fbd05a98c
Udayan Yaragattikar <66424882+udayan-y21@users.noreply.github.com>
2020-09-10 19:08:49 +0530

    Update build.sh

--- cicd/build.sh
+++ cicd/build.sh
@@ -247,10 +247,6 @@ BUILD_END_TIME=$(date +%s)
 echo "CSM RPMs ..."
 find $BASE_DIR -name *.rpm
 
-echo "~~~~~~~~~~~~~~~~~~Echoing cortx-prvsnr BEFORE AUXIALLARY~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-rpm -qa | grep cortx-prvsnr
-python3 -c "import provisioner; print(provisioner.__file__)"
-
 [ "$INTEGRATION" == true ] && {
     INTEGRATION_TEST_START=$(date +%s)
     bash "$BASE_DIR/cicd/auxiliary/csm_cicd.sh" "$DIST/rpmbuild/RPMS/x86_64" "$BASE_DIR" "$CSM_PATH"    
