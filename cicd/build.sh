4aef24ff6e710b349b8f6cab8fd563ecb83e4ae9
Udayan Yaragattikar <66424882+udayan-y21@users.noreply.github.com>
2020-09-11 13:11:06 +0530

    Adding python36-cortx-prvsnr to build.sh

--- cicd/build.sh
+++ cicd/build.sh
@@ -142,7 +142,7 @@ if [ "$DEV" == true ]; then
 else
     pip3 install --upgrade pip
     pip3 install pyinstaller==3.5
-    yum install -y eos-py-utils cortx-prvsnr
+    yum install -y eos-py-utils cortx-prvsnr python36-cortx-prvsnr
 
     # Check python package
     req_file=$BASE_DIR/cicd/pyinstaller/requirment.txt
