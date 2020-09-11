dd79034bcb635d5853043849b8027eb3616b634d
Naval Patel <naval.patel@seagate.com>
2020-09-11 06:26:08 -0600

    Commented cortx-py-utils as it is included by special job
    Signed-off-by: Naval Patel <naval.patel@seagate.com>

--- cicd/build.sh
+++ cicd/build.sh
@@ -143,7 +143,7 @@ else
     pip3 install --upgrade pip
     pip3 install pyinstaller==3.5
     # add cortx-py-utils below
-    yum install -y cortx-py-utils 
+    #yum install -y cortx-py-utils 
     yum install -y python36-cortx-prvsnr
 
     # Check python package
