84bf138153000ecfc31123c93e4ed3d98b100b27
Naval Patel <naval.patel@seagate.com>
2020-09-17 22:45:27 -0600

    Moved entry for statsd cleanup also into es_logrotate.cron
    Signed-off-by: Naval Patel <naval.patel@seagate.com>

--- csm/conf/setup.py
+++ csm/conf/setup.py
@@ -363,30 +363,6 @@ class Setup:
         self._cluster.init(args['f'])
         CsmApi.set_cluster(self._cluster)
 
-    def _cleanup_job(self, reset=False):
-        """
-        Check if csm_cleanup present is csm
-            : If csm_cleanup present then configure cronjob
-            : If csm_cleanup not present then through error
-        """
-        _proc = SimpleProcess("crontab -u " +self._user+ " -l")
-        _output, _err, _rc = _proc.run(universal_newlines=True)
-        if not reset:
-            if "no crontab" not in _err:
-                for job in _output.split('\n'):
-                    if const.CSM_CRON_JOB in job:
-                        return
-            with open("/tmp/csm.cron", "w") as fi:
-                if "no crontab" not in _err:
-                    fi.write(_output)
-                fi.write("0 1 * * *    {}\n".format(const.CSM_CRON_JOB))
-            _output = Setup._run_cmd("crontab -u " +self._user+ " /tmp/csm.cron")
-            os.remove("/tmp/csm.cron")
-        else:
-            if self._is_user_exist():
-                if "no crontab" not in _err:
-                    Setup._run_cmd("crontab -u " +self._user+ " -r")
-
     def _log_cleanup(self):
         """
         Delete all logs
@@ -444,6 +420,7 @@ class Setup:
     def _rsyslog_common(self):
         """
         Configure common rsyslog and logrotate
+        Alson cleanup statsd
         """
         if os.path.exists(const.LOGROTATE_DIR):
             Setup._run_cmd("cp -f " +const.CLEANUP_LOGROTATE_PATH+ " " +const.LOGROTATE_PATH)
@@ -704,7 +681,6 @@ class CsmSetup(Setup):
         Perform post-install for csm
             : Configure csm user
             : Add Permission for csm user
-            : Add cronjob for csm cleanup
         Post install is used after just all rpms are install but
         no service are started
         """
@@ -712,7 +688,6 @@ class CsmSetup(Setup):
             self._verify_args(args)
             self._config_user()
             self.set_unsupported_feature_info()
-            self._cleanup_job()
             self._configure_system_auto_restart()
             
         except Exception as e:
@@ -781,7 +756,6 @@ class CsmSetup(Setup):
                 self.ConfigServer.stop()
                 self._log_cleanup()
                 self._config_user_permission(reset=True)
-                self._cleanup_job(reset=True)
                 self.Config.delete()
                 self._config_user(reset=True)
             else:
