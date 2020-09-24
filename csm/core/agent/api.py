3809fce8def9aea3192527cdfe7a7d1335aafb23
Soniya <soniya.moholkar@seagate.com>
2020-09-23 20:48:19 -0600

    test changes for build fix

--- csm/core/agent/api.py
+++ csm/core/agent/api.py
@@ -32,7 +32,6 @@ from csm.common.observer import Observable
 from csm.common.payload import *
 from csm.common.conf import Conf, ConfSection, DebugConf
 from cortx.utils.log import Log
-from cortx.utils.product_features import unsupported_features
 from csm.common.payload import Json
 from csm.common.services import Service
 from csm.core.blogic import const
@@ -113,27 +112,7 @@ class CsmRestApi(CsmApi, ABC):
 
         CsmRestApi._app.on_startup.append(CsmRestApi._on_startup)
         CsmRestApi._app.on_shutdown.append(CsmRestApi._on_shutdown)
-        CsmRestApi.update_roles_permission()
-
-    @staticmethod
-    def update_roles_permission():
-        # Remove lyve_pilot permission if it is not supported
-        try:
-            roles = Json(const.ROLES_MANAGEMENT).load()
-            unsupported_feature_instance = unsupported_features.UnsupportedFeaturesDB()
-            loop = asyncio.get_event_loop()
-            feature_supported = loop.run_until_complete(unsupported_feature_instance.is_feature_supported(const.CSM_COMPONENT_NAME, const.LYVE_PILOT))
-
-            if not feature_supported:
-                Log.debug(f"{const.LYVE_PILOT} is not supported.")
-                for permissions in roles.values():
-                    if permissions.get(const.PERMISSIONS).get(const.LYVE_PILOT):
-                        del permissions.get(const.PERMISSIONS)[const.LYVE_PILOT]
-                        Log.debug(f"{const.LYVE_PILOT} permissions removed.")
-                Json(const.ROLES_MANAGEMENT).dump(roles)
-        except Exception as e_:
-            Log.error(f"Error occurred while updating permissions: {e_}")
-
+        
     @staticmethod
     def is_debug(request) -> bool:
         return 'debug' in request.rel_url.query
