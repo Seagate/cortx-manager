971946a42cac346a5ba78bfa382572ff81f8f57c
Alexander Voronov <alexander.voronov@seagate.com>
2020-09-25 07:37:53 +0000

    EOS-13625: fix audit logging of session credentials
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>

--- csm/core/agent/api.py
+++ csm/core/agent/api.py
@@ -151,7 +151,8 @@ class CsmRestApi(CsmApi, ABC):
             resp["message"] = f'{str(err)}'
 
         audit = CsmRestApi.http_request_to_log_string(request)
-        if hasattr(request,"session"):
+        if (getattr(request, "session", None) is not None
+                and getattr(request.session, "credentials", None) is not None):
             Log.audit(f'User: {request.session.credentials.user_id} '
                       f'{audit} RC: {resp["error_code"]}')
         else:
