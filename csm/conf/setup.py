cd15b8108dabf6fafcef4c01e4e711eb8e77ec3b
Prathamesh Rodi <prathamesh.rodi@seagate.com>
2021-01-08 11:40:11 +0530

    EOS-16207 Changes for Csm Init Command to be Moved to New Class and Removing old Code.
    Signed-off-by: Prathamesh Rodi <prathamesh.rodi@seagate.com>

--- csm/conf/setup.py
+++ csm/conf/setup.py
@@ -557,43 +557,6 @@ class Setup:
             Log.error("Unable to fetch system node ids info.")
             raise CsmSetupError(f"Unable to fetch system node ids info.")
 
-    def _set_rmq_cluster_nodes(self):
-        """
-        This method gets the nodes names of the the rabbitmq cluster and writes in the config.
-        """
-        Log.info("Setting RMQ cluster nodes in config")
-        nodes = []
-        nodes_found = False
-        try:
-            for count in range(0, const.RMQ_CLUSTER_STATUS_RETRY_COUNT):
-                cmd_output = Setup._run_cmd(const.RMQ_CLUSTER_STATUS_CMD)
-                # The code below is used to parse RMQ 3.3.5 "cluster_status" command output
-                for line in cmd_output[0].split('\n'):
-                    if const.RUNNING_NODES in line:
-                        nodes = re.findall(r"rabbit@([-\w]+)", line)
-                        nodes_found = True
-                if nodes_found:
-                    break
-                # The code below is used to parse CLI output for RMQ 3.8.9 or above
-                result = re.search(
-                    f"{const.RUNNING_NODES_START_TEXT}.*?{const.RUNNING_NODES_STOP_TEXT}",
-                    cmd_output[0], re.DOTALL)
-                if result is not None:
-                    nodes = re.findall(r"rabbit@([-\w]+)", result.group(0))
-                    break
-                time.sleep(2**count)
-            if nodes:
-                conf_key = f"{const.CHANNEL}.{const.RMQ_HOSTS}"
-                Log.debug(f"Saving nodes:{nodes} to conf_key:{conf_key} in Config")
-                Conf.set(const.CSM_GLOBAL_INDEX, conf_key, nodes)
-                Conf.save(const.CSM_GLOBAL_INDEX)
-            else:
-                Log.error("Unable to fetch RMQ cluster nodes info.")
-                raise CsmSetupError("Unable to fetch RMQ cluster nodes info.")
-        except Exception as e:
-            Log.error(f"Setting RMQ cluster nodes failed. {e} - {str(traceback.print_exc())}")
-            raise CsmSetupError(f"Setting RMQ cluster nodes failed. {e} - {str(traceback.print_exc())}")
-
     @staticmethod
     def _get_minion_id():
         """
@@ -886,42 +849,6 @@ class CsmSetup(Setup):
         except Exception as e:
             Log.error(f"csm_setup config failed. Error: {e} - {str(traceback.print_exc())}")
             raise CsmSetupError(f"csm_setup config failed. Error: {e} - {str(traceback.print_exc())}")
-
-    def init(self, args):
-        """
-        Check and move required configuration file
-        Init is used after all dependency service started
-        """
-        Log.info("Triggering csm_setup post_install init")
-        cls = self.__class__
-        try:
-            self._verify_args(args)
-            self.Config.load()
-            self._config_user_permission()
-            if not self._replacement_node_flag:
-                self._set_rmq_cluster_nodes()
-                #TODO: Adding this implementation in try..except block to avoid build failure
-                # This workaround will be fixed once JIRA ticket #10551 is resolved
-                try:
-                    self._set_rmq_node_id()
-                except Exception as e:
-                    Log.error(f"Failed to fetch system node ids info from provisioner cli.- {e}")
-                minion_id = cls._get_minion_id()
-                data_nw = cls._get_data_nw_info(minion_id)
-                cls._set_db_host_addr('consul', data_nw.get('roaming_ip', 'localhost'))
-                cls._set_db_host_addr('es', data_nw.get('pvt_ip_addr', 'localhost'))
-            self.ConfigServer.reload()
-            self._rsyslog()
-            self._logrotate()
-            self._rsyslog_common()
-            Setup._set_fqdn_for_nodeid()
-            Setup._set_healthmap_path()
-            ha_check = Conf.get(const.CSM_GLOBAL_INDEX, "HA.enabled")
-            if ha_check:
-                self._config_cluster(args)
-        except Exception as e:
-            Log.error(f"csm_setup init failed. Error: {e} - {str(traceback.print_exc())}")
-            raise CsmSetupError(f"csm_setup init failed. Error: {e} - {str(traceback.print_exc())}")
 
     def reset(self, args):
         """
