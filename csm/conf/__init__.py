372cf29f37826b28d2897b98b0094bd155037733
Prathamesh Rodi <prathamesh.rodi@seagate.com>
2021-01-14 10:24:20 +0530

    EOS-16153 Testing Build Failure.
    Signed-off-by: Prathamesh Rodi <prathamesh.rodi@seagate.com>

--- csm/conf/__init__.py
+++ csm/conf/__init__.py
@@ -13,5 +13,9 @@
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
-
+from csm.conf.post_install import PostInstall
+from csm.conf.configure import Configure
+from csm.conf.init import Init
+from csm.conf.reset import Reset
+from csm.conf.refresh_config import RefreshConfig
 
