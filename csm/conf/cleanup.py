b600c7796d3c66755954a118aa51470c586b0a29
Prathamesh Rodi <66405797+prathameshrodi@users.noreply.github.com>
2021-01-29 14:43:00 +0530

    EOS-17083 Changes for Base for Deleting Elasticsearch and Consul Data.
    Signed-off-by: Prathamesh Rodi <66405797+prathameshrodi@users.noreply.github.com>

--- csm/conf/cleanup.py
+++ csm/conf/cleanup.py
@@ -18,7 +18,10 @@ from csm.conf.uds import UDSConfigGenerator
 from cortx.utils.log import Log
 from cortx.utils.conf_store.conf_store import Conf
 from cortx.utils.kv_store.error import KvError
+from cortx.utils.data.db.db_provider import (DataBaseProvider, GeneralConfig)
+from cortx.utils.errors import DataAccessExternalError
 from csm.core.blogic import const
+from csm.common.payload import Yaml
 from csm.core.providers.providers import Response
 from csm.common.errors import CSM_OPERATION_SUCESSFUL
 
@@ -29,16 +32,24 @@ class Cleanup(Setup):
     def __init__(self):
         super(Cleanup, self).__init__()
         Log.info("Running Cleanup for CSM.")
+        self._db = None
 
-    async def execute(self):
+    async def execute(self, command):
+        database_cleanup = (command.options.get("database_cleanup",
+                                                False) == 'true')
         try:
             Conf.load(const.CSM_GLOBAL_INDEX, const.CSM_SOURCE_CONF_URL)
+            Conf.load(const.DATABASE_INDEX, f"yaml://{const.DATABASE_CONF}")
         except KvError as e:
             Log.error(f"Loading csm.conf to conf store failed {e}")
             raise CsmSetupError("Failed to Load CSM Configuration File.")
         self._log_cleanup()
-        self.Config.delete()
         UDSConfigGenerator.delete()
+        if database_cleanup:
+            self.load_db()
+            self.es_cleanup()
+            self.consul_cleanup()
+        self.Config.delete()
         return Response(output=const.CSM_SETUP_PASS, rc=CSM_OPERATION_SUCESSFUL)
 
     def _log_cleanup(self):
@@ -48,3 +59,43 @@ class Cleanup(Setup):
         Log.info("Delete all logs")
         log_path = Conf.get(const.CSM_GLOBAL_INDEX, "Log>log_path")
         Setup._run_cmd("rm -rf " +log_path)
+
+    def load_db(self):
+        """
+        Load Database Provider from database.yaml
+        :return:
+        """
+        Log.info("Loading Database Provider.")
+        conf = GeneralConfig(Yaml(const.DATABASE_CLI_CONF).load())
+        self._db = DataBaseProvider(conf)
+
+    def _db_cleanup(self):
+        """
+        :return:
+        """
+        self._es_host = Conf.get(const.DATABASE_INDEX,
+                                 "databases>es_db>config>host")
+        for each_model in Conf.get(const.DATABASE_INDEX, "models"):
+            if each_model.get("database") == "es_db":
+                self._es_cleanup(each_model.get("collection"))
+            if each_model.get("database") == "consul_db":
+                self._es_cleanup(each_model.get("collection"))
+
+    def _es_cleanup(self, es_collection_name):
+        """
+        Clean up all the CSM Elasticsearch Data.
+        :param es_collection_name: Elasticsearch Collection Name to be Deleted.
+        :return:
+        """
+        Log.info(f"Cleaning up Collection {es_collection_name}")
+        pass
+
+    def consul_cleanup(self, consul_collection_name):
+        """
+        Cleanup all the CSM Consul Data.
+        :param consul_collection_name: Consul Collection Name to be Deleted.
+        :return:
+        """
+        Log.info(f"Cleaning up Collection {consul_collection_name}")
+        pass
+
