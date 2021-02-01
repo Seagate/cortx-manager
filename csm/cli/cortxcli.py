48bf92c3377e08d0048efc61b24643103c375779
Alexander Voronov <alexander.voronov@seagate.com>
2021-02-01 08:21:54 +0000

    fix typo in cortxcli's ArgumentError class (self.messgae -> self.message)
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>

--- csm/cli/cortxcli.py
+++ csm/cli/cortxcli.py
@@ -28,7 +28,7 @@ class ArgumentError(argparse.ArgumentError):
     def __init__(self, rc, message, argument=None):
         super(ArgumentError, self).__init__(argument, message)
         self.rc = rc
-        self.messgae = message
+        self.message = message
 
     def __str__(self):
         return f"{self.rc}: {self.message}"
