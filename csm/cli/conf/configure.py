544b1915ae8fdcbceae59511cbd926835355fd4e
Alexander Voronov <alexander.voronov@seagate.com>
2021-03-02 05:36:04 -0700

    EOS-18381: implement the 'cli_setup config' command
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>

--- csm/cli/conf/configure.py
+++ csm/cli/conf/configure.py
@@ -13,9 +13,12 @@
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
-
+import os
+import traceback
+from cortx.utils.conf_store.conf_store import Conf
+from cortx.utils.kv_store.error import KvError
 from cortx.utils.log import Log
-from csm.conf.setup import Setup
+from csm.conf.setup import Setup, CsmSetupError
 from csm.core.blogic import const
 from csm.core.providers.providers import Response
 from csm.common.errors import CSM_OPERATION_SUCESSFUL
@@ -40,4 +43,39 @@ class Configure(Setup):
         """
 
         Log.info("Executing Config for CORTX CLI")
+        try:
+            Conf.load(const.CONSUMER_INDEX, command.options.get(const.CONFIG_URL))
+            Conf.load(const.CORTXCLI_GLOBAL_INDEX, const.CORTXCLI_CONF_FILE_URL)
+            self._set_deployment_mode()
+            self.cli_create(command)
+        except KvError as e:
+            err_msg = f"Failed to load the configuration: {e}"
+            Log.error(err_msg)
+            raise CsmSetupError(err_msg)
+        except Exception as e:
+            err_msg = (f"cli_setup config command failed. Error: "
+                       f"{e} - {str(traceback.format_exc())}")
+            Log.error(err_msg)
+            raise CsmSetupError(err_msg)
         return Response(output=const.CSM_SETUP_PASS, rc=CSM_OPERATION_SUCESSFUL)
+
+    def cli_create(self, command):
+        """
+        Create the CortxCli configuration file on the required location.
+        """
+
+        os.makedirs(const.CORTXCLI_PATH, exist_ok=True)
+        os.makedirs(const.CORTXCLI_CONF_PATH, exist_ok=True)
+        Setup._run_cmd(
+            f"setfacl -R -m u:{self._user}:rwx {const.CORTXCLI_PATH}")
+        Setup._run_cmd(
+            f"setfacl -R -m u:{self._user}:rwx {const.CORTXCLI_CONF_PATH}")
+        Conf.set(const.CORTXCLI_GLOBAL_INDEX,
+                 f"{const.CORTXCLI_SECTION}>{const.CSM_AGENT_HOST_PARAM_NAME}" ,
+                 command.options.get(const.ADDRESS_PARAM, "127.0.0.1"))
+        if self._is_env_dev:
+            Conf.set(const.CORTXCLI_GLOBAL_INDEX,
+                     f"{const.DEPLOYMENT}>{const.MODE}", const.DEV)
+        Conf.save(const.CORTXCLI_GLOBAL_INDEX)
+        Setup._run_cmd(
+            f"cp -rn {const.CORTXCLI_SOURCE_CONF_PATH} {const.ETC_PATH}")
