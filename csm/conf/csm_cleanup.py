0861fc5ea52be5bfa848dbf667cd22e134f0f54d
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2021-03-07 00:21:34 -0700

    Adding python3 shebang in csm executables

--- csm/conf/csm_cleanup.py
+++ csm/conf/csm_cleanup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # CORTX-CSM: CORTX Management web and CLI interface.
 # Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
 # This program is free software: you can redistribute it and/or modify
