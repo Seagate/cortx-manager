06a9ad9768e5c137538e45f3f34ac7fd8f93a949
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2021-03-08 06:07:06 -0700

    adding csm path to sys.path

--- csm/conf/csm_cleanup.py
+++ csm/conf/csm_cleanup.py
@@ -154,7 +154,7 @@ def add_cleanup_subcommand(main_parser):
 
 if __name__ == '__main__':
     sys.path.append(os.path.join(os.path.dirname(pathlib.Path(__file__)), '..', '..'))
-    sys.path.append("/opt/seagate/cortx")
+    sys.path.append(os.path.join(os.path.dirname(pathlib.Path(os.path.realpath(sys.argv[0]))), '..', '..'))
     from cortx.utils.log import Log
     from cortx.utils.cleanup.es_data_cleanup import esCleanup
     from cortx.utils.conf_store.conf_store import Conf, ConfSection, DebugConf
