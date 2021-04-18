d02e5e61c62e07def55f219815a105a3722d6321
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2021-04-18 09:20:54 -0600

    Fixing unsupported feature issue for LyvePilot
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>

--- csm/core/agent/api.py
+++ csm/core/agent/api.py
@@ -121,9 +121,10 @@ class CsmRestApi(CsmApi, ABC):
     def update_roles_permission():
         # Remove lyve_pilot permission if it is not supported
         try:
+            _loop = asyncio.get_event_loop()
             roles = Json(const.ROLES_MANAGEMENT).load()
             unsupported_feature_instance = unsupported_features.UnsupportedFeaturesDB()
-            feature_supported = CsmRestApi._app._loop.run_until_complete(
+            feature_supported = _loop.run_until_complete(
                 unsupported_feature_instance.is_feature_supported(
                     const.CSM_COMPONENT_NAME, const.LYVE_PILOT))
 
