f02a48123354f41f1af59d210b01f7af2f0c3d4e
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2021-05-17 00:17:08 -0600

    Update setup.yaml
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>

--- csm/core/services/usl_certificate_manager.py
+++ csm/core/services/usl_certificate_manager.py
@@ -1,5 +1,4 @@
 # CORTX-CSM: CORTX Management web and CLI interface.
-# CORTX-CSM: CORTX Management web and CLI interface.
 # Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
