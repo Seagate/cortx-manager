0c41bf99751bf11351ea0c314055d4463a1f151a
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2021-05-24 05:11:16 -0600

    Addressing comments
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>

--- csm/cli/cli_validators.py
+++ csm/cli/cli_validators.py
@@ -16,10 +16,10 @@
 import errno
 from cortx.utils.log import Log
 from cortx.utils.cli_framework.errors import CliError
-from cortx.utils.validator.cli_validators import CommonValidators
+from cortx.utils.validator.cli_validators import CliValidators
 from csm.core.controllers.validators import BucketNameValidator
 
-class Validators(CommonValidators):
+class Validators(CliValidators):
 
     """CLI Validators Class"""
     @staticmethod
