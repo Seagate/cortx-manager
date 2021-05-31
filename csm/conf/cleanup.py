3760801e1e5e977bd143fc290f2d5984bf2d61a1
Udayan Yaragattikar <66424882+udayan-y21@users.noreply.github.com>
2021-05-31 15:48:03 +0530

    EOS-20355: Cleanup command for csm_setup (#479)
    * EOS-20347 PAM AUTH POC (#464)
    
    * Correction in csm_agent login url and other minor changes
    
    Signed-off-by: rohitkolapkar <rohit.j.kolapkar@seagate.com>
    
    * Curl option for failed API response has set to TRUE
    
    Signed-off-by: rohitkolapkar <rohit.j.kolapkar@seagate.com>
    
    * PAM readme.updated
    
    Signed-off-by: rohitkolapkar <rohit.j.kolapkar@seagate.com>
    
    * Password prompt issue resolved
    
    Signed-off-by: rohitkolapkar <rohit.j.kolapkar@seagate.com>
    
    * PAM module renamed to pam_cortx
    
    Signed-off-by: rohitkolapkar <rohit.j.kolapkar@seagate.com>
    
    * PAM readme updated
    
    Signed-off-by: rohitkolapkar <rohit.j.kolapkar@seagate.com>
    
    * Removed unnecessary comments
    
    Signed-off-by: rohitkolapkar <rohit.j.kolapkar@seagate.com>
    
    * Trying to fix codacy issue
    
    Signed-off-by: rohitkolapkar <rohit.j.kolapkar@seagate.com>
    
    Co-authored-by: kupranay <66459168+kupranay@users.noreply.github.com>
    
    * Cleanup command for csm_setup
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * Modifications for cleanup command
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * t-main updation (#484)
    
    Signed-off-by: Pawan Kumar Srivastava <pawan.kumarsrivastava@seagate.com>
    
    * Modifications for Cleanup command for csm-setup
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * Modifications for Cleanup command for csm-setup
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * Cleanup cmd modifications
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * Minor modification
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * Addressing comments
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * Resolve conflicts
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    Co-authored-by: Rohit Kolapkar <rohit.j.kolapkar@seagate.com>
    Co-authored-by: kupranay <66459168+kupranay@users.noreply.github.com>
    Co-authored-by: Pawan Kumar Srivastava <66408908+pawankumarsrivastava@users.noreply.github.com>

---
+++ csm/conf/cleanup.py
@@ -0,0 +1,56 @@
+# CORTX-CSM: CORTX Management web and CLI interface.
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+from cortx.utils.conf_store.conf_store import Conf
+from cortx.utils.log import Log
+from csm.core.blogic import const
+from csm.core.providers.providers import Response
+from cortx.utils.kv_store.error import KvError
+from csm.common.errors import CSM_OPERATION_SUCESSFUL
+from csm.conf.setup import CsmSetupError, Setup
+
+class Cleanup(Setup):
+    """
+    Delete all the CSM generated files and folders
+    """
+
+    def __init__(self):
+        super(Cleanup, self).__init__()
+        Log.info("Triggering Cleanup for CSM.")
+
+    async def execute(self, command):
+        """
+        :param command:
+        :return:
+        """
+        try:
+            Log.info("Loading configuration")
+            Conf.load(const.CSM_GLOBAL_INDEX, const.CSM_CONF_URL)
+        except KvError as e:
+            Log.error(f"Configuration Loading Failed {e}")
+        self.directory_cleanup()
+        return Response(output=const.CSM_SETUP_PASS, rc=CSM_OPERATION_SUCESSFUL)
+
+    def directory_cleanup(self):
+        files_directory_list = [
+            const.RSYSLOG_PATH,
+            const.CSM_LOGROTATE_DEST,
+            const.DEST_CRON_PATH,
+            const.CSM_CONF_PATH,
+        ]
+
+        for dir_path in files_directory_list:
+            Log.info(f"Deleteing path :{dir_path}")
+            Setup._run_cmd(f"rm -rf {dir_path}")
