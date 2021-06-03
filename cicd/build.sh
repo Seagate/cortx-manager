bc58842123a6e3cf7705cb9a122de44ee7d61303
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2021-06-03 17:19:03 +0000

    test commit
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>

--- cicd/build.sh
+++ cicd/build.sh
@@ -14,6 +14,7 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
 set -e
+set -x
 BUILD_START_TIME=$(date +%s)
 BASE_DIR=$(realpath "$(dirname $0)/..")
 PROG_NAME=$(basename $0)
@@ -220,9 +221,12 @@ if [ "$COMPONENT" == "all" ] || [ "$COMPONENT" == "backend" ]; then
     cp -rf "$BASE_DIR/templates" "$DIST/csm/"
     cp -rf "$BASE_DIR/test/" "$DIST/csm"
     cp -rf "$BASE_DIR/csm/cli/schema/csm_setup.json" "$DIST/csm/schema/"
-
-    mv -f "$BASE_DIR/csm/conf/miniprovisioning_templates/*" "$DIST/csm/conf/"
-    rm -rf "$BASE_DIR/csm/conf/miniprovisioning_templates/"
+    ls -la "$BASE_DIR/csm"
+    ls -la "$BASE_DIR/csm/conf"
+    ls -la "$DIST/csm"
+    ls -la "$DIST/csm/conf"
+    # mv -f "$BASE_DIR/csm/conf/miniprovisioning_templates/*" "$DIST/csm/conf/"
+    # rm -rf "$BASE_DIR/csm/conf/miniprovisioning_templates/"
 
     # Copy executables files
     cp -f "$BASE_DIR/csm/core/agent/csm_agent.py" "$DIST/csm/lib/csm_agent"
