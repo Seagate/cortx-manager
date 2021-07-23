169d2729cc2ec77018ab4ab0dd94f378201bf8d1
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2021-07-23 09:40:57 +0000

    Firsttime reset password and Admin creation through miniprovisioning
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>

--- csm/conf/configure.py
+++ csm/conf/configure.py
@@ -29,7 +29,7 @@ from cortx.utils.validator.v_network import NetworkV
 from cortx.utils.validator.v_consul import ConsulV
 from cortx.utils.validator.v_elasticsearch import ElasticsearchV
 from csm.core.data.models.users import User
-from csm.core.services.users import UserManager
+from csm.core.services.users import CsmUserService, UserManager
 from cortx.utils.data.db.db_provider import DataBaseProvider, GeneralConfig
 from csm.core.controllers.validators import PasswordValidator, UserNameValidator
 
@@ -272,26 +272,26 @@ class Configure(Setup):
         # TODO confstore keys can be changed.
         Log.info("Creating cluster admin account")
         cluster_admin_user = Conf.get(const.CONSUMER_INDEX,
-                                    "cortx>software>cluster_credential>admin",
+                                    "cortx>software>cluster_credential>user",
                                     const.DEFAULT_CLUSTER_ADMIN_USER)
-        cluster_admin_pass = Conf.get(const.CONSUMER_INDEX,
-                                    "cortx>software>cluster_credential>password",
+        cluster_admin_secret = Conf.get(const.CONSUMER_INDEX,
+                                    "cortx>software>cluster_credential>secret",
                                     const.DEFAULT_CLUSTER_ADMIN_PASS)
-        cluster_admin_email = Conf.get(const.CONSUMER_INDEX,
-                                    "cortx>software>cluster_credential>email",
+        cluster_admin_emailid = Conf.get(const.CONSUMER_INDEX,
+                                    "cortx>software>cluster_credential>emailid",
                                     const.DEFAULT_CLUSTER_ADMIN_EMAIL)
-        #TODO Username and Password Validation can change
+
         UserNameValidator()(cluster_admin_user)
-        PasswordValidator()(cluster_admin_pass)
+        PasswordValidator()(cluster_admin_secret)
 
         conf = GeneralConfig(Yaml(const.DATABASE_CONF).load())
         db = DataBaseProvider(conf)
         usr_mngr = UserManager(db)
+        usr_service = CsmUserService(usr_mngr)
         if (not self.force_action) and \
-            ((await usr_mngr.count_admins() > 0) or \
-            (await usr_mngr.get(cluster_admin_user))):
+            (await usr_service.validate_cluster_admin_create(cluster_admin_user)):
             Log.console("WARNING: Cortx cluster admin already created.\n"
-                        "Please use '-f' option to create admin account forcefully.")
+                        "Please use '-f' option to create admin user forcefully.")
             return None
 
         if self.force_action and await usr_mngr.get(cluster_admin_user):
@@ -299,10 +299,6 @@ class Configure(Setup):
             await usr_mngr.delete(cluster_admin_user)
 
         Log.info(f"Creating cluster admin: {cluster_admin_user}")
-        user = User.instantiate_csm_user(user_id=cluster_admin_user,
-                                            password=cluster_admin_pass,
-                                            role=const.CSM_SUPER_USER_ROLE,
-                                            email=cluster_admin_email,
-                                            alert_notification=True)
-        Log.debug(f"Creating user: \n{user.__dict__}")
-        await usr_mngr.create(user)
+        await usr_service.create_cluster_admin(cluster_admin_user, 
+                                                cluster_admin_secret,
+                                                cluster_admin_emailid)
