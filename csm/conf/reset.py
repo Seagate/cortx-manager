e404e41aa5fb2ac2a91f66ffe563116f5de93c01
Udayan Yaragattikar <66424882+udayan-y21@users.noreply.github.com>
2021-08-10 14:12:37 +0530

    EOS-22899: F-97D: cleanup pre-factory- csm_agent (#598)
    * Pre-factory modificaions  in cleanup command
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * Cleanup prefactory modifications
    
    * Codacy issue resolved
    
    * minor modifications
    
    * Minor modifications

--- csm/conf/reset.py
+++ csm/conf/reset.py
@@ -29,7 +29,7 @@ from cortx.utils.data.db.consul_db.storage import CONSUL_ROOT
 
 class Reset(Setup):
     """
-    Reset csm Configuration.
+    Reset Csm User Data.
     """
     def __init__(self):
         super(Reset, self).__init__()
@@ -54,6 +54,9 @@ class Reset(Setup):
         return Response(output=const.CSM_SETUP_PASS, rc=CSM_OPERATION_SUCESSFUL)
 
     def disable_and_stop_service(self):
+        '''
+        Stop and Disable CSM services
+        '''
         for each_service in [const.CSM_AGENT_SERVICE, const.CSM_WEB_SERVICE]:
             try:
                 service_obj = Service(each_service)
@@ -76,6 +79,9 @@ class Reset(Setup):
                 Log.warn(f"{each_service} not available: {e}")
 
     def directory_cleanup(self):
+        '''
+        Remove CSM Data directories
+        '''
         Log.info("Deleting files and folders")
 
         files_directory_list = [
@@ -88,20 +94,27 @@ class Reset(Setup):
             Setup._run_cmd(f"rm -rf {_path}")
 
     def reset_logs(self):
+        '''
+        Reset CSM logs. Removal will be part of Cleanup
+        '''
         Log.info("Reseting log files")
         log_dir = Conf.get(const.CSM_GLOBAL_INDEX, 'Log>log_path')
         csm_log_files = ["csm_agent.log", "csm_middleware.log", "cortxcli.log"]
         all_log_files = os.listdir(log_dir)
         for each_file in all_log_files:
             if each_file in csm_log_files:
+                # Reseting current log files
                 _file = os.path.join(log_dir, each_file)
                 Setup._run_cmd(f"truncate -s 0 {_file}")
             else:
+                # Remove other files.i.e. older log tar.gz files etc
                 _file = os.path.join(log_dir, each_file)
                 Setup._run_cmd(f"rm -rf {each_file}")
 
     async def db_cleanup(self):
-
+        '''
+        Remove User created ES and Consul data
+        '''
         port = Conf.get(const.DATABASE_INDEX, 'databases>es_db>config>port')
         self._es_db_url = (f"http://localhost:{port}/")
         port = Conf.get(const.DATABASE_INDEX, 'databases>consul_db>config>port')
