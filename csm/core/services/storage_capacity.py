abe5b6231d33e37fd5c491371f7eea91f0bf3107
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2022-01-11 09:17:56 +0000

    Expose REST API for fetching Degraded Object Byte count
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>

--- csm/core/services/storage_capacity.py
+++ csm/core/services/storage_capacity.py
@@ -14,12 +14,15 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
 import json
+import aiohttp
+from aiohttp.client import ClientSession
 from csm.common.process import SimpleProcess,AsyncioSubprocess
 from cortx.utils.log import Log
 from csm.common.services import ApplicationService
 from csm.core.blogic import const
 from csm.common.errors import CsmInternalError, CsmError
 from typing import Callable, Dict, Any
+from cortx.utils.conf_store.conf_store import Conf
 
 class StorageCapacityService(ApplicationService):
     """
@@ -84,3 +87,24 @@ class StorageCapacityService(ApplicationService):
                                                              int(capacity_info[const.TOTAL_SPACE])) * 100),round_off_value)
         formatted_output[const.UNIT] = unit
         return formatted_output
+
+    async def request(self, session: ClientSession, method, url):
+        async with session.request(url=url, method=method) as resp:
+            return await resp.json(), resp.status
+
+    async def get_cluster_data(self, data_filter=None):
+        #TODO: Use data_filter for filtering out the data, once integrated in hctl api
+        url = Conf.get(const.CSM_GLOBAL_INDEX,const.CAPACITY_MANAGMENT_HCTL_SVC_ENDPOINT) + \
+            Conf.get(const.CSM_GLOBAL_INDEX,const.CAPACITY_MANAGMENT_HCTL_CLUSTER_API)
+        method = const.GET
+        if data_filter:
+            url = url+data_filter
+        Log.info(f"Request {url} for cluster data")
+        async with aiohttp.ClientSession() as session:
+            try:
+                response, status = await self.request(session, method, url)
+            except Exception as e:
+                Log.error(f"Error in obtaining response from {url}: {e}")
+                raise CsmInternalError(f"Error in obtaining response from {url}: {e}")
+        Log.debug(f"Response: {response}, Status:{status} for url :{url}")
+        return response, status
