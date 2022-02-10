f475a4bb25bd75d2b43886d4e2af630170955f59
abhijit1patil <abhijit.1.patil@seagate.com>
2022-02-10 07:58:58 -0700

    Review comments handled
    Signed-off-by: abhijit1patil <abhijit.1.patil@seagate.com>

--- csm/core/controllers/rgw/s3/users.py
+++ csm/core/controllers/rgw/s3/users.py
@@ -50,7 +50,6 @@ class CreateKeySchema(Schema):
     """S3 Add Access Key."""
 
     uid = fields.Str(data_key=const.RGW_JSON_UID, required=True)
-    subuser = fields.Str(data_key=const.RGW_JSON_SUBUSER, missing=None)
     key_type = fields.Str(data_key=const.RGW_JSON_KEY_TYPE, missing=None,
                     validate=validate.OneOf(['s3']))
     access_key = fields.Str(data_key=const.RGW_JSON_ACCESS_KEY, missing=None)
@@ -69,7 +68,6 @@ class RemoveKeySchema(Schema):
     """S3 Remove Key ."""
     access_key = fields.Str(data_key=const.RGW_JSON_ACCESS_KEY, required=True)
     uid = fields.Str(data_key=const.RGW_JSON_UID, missing=None)
-    subuser = fields.Str(data_key=const.RGW_JSON_SUBUSER, missing=None)
     key_type = fields.Str(data_key=const.RGW_JSON_KEY_TYPE, missing=None,
                     validate=validate.OneOf(['s3']))
     
@@ -115,7 +113,7 @@ class S3IAMUserListView(S3BaseView):
             return CsmResponse(response)
 
 @CsmView._app_routes.view("/api/v2/s3/iam/users/keys")
-class AccessKeyListView(S3BaseView):
+class S3IAMUserKeyView(S3BaseView):
     """
     S3 IAM User List View for REST API implementation.
 
