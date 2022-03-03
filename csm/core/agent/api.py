eee667f2be55c07e6116f210b36786f7321e37a2
Pranali04796 <pranali.ugale@seagate.com>
2022-03-02 23:16:09 -0700

    CORTX-28027:Update status api to query for bytecount
    Signed-off-by: Pranali04796 <pranali.ugale@seagate.com>

--- csm/core/agent/api.py
+++ csm/core/agent/api.py
@@ -390,11 +390,10 @@ class CsmRestApi(CsmApi, ABC):
                             lower_key.find("secret") > -1    :
                             del(request_body[key])
             payload = json.dumps(request_body)
-            # TODO: This will be uncommented when unsupported feature is enable
-            #try:
-                #await CsmRestApi.check_for_unsupported_endpoint(request)
-            #except DataAccessError as e:
-                #Log.warn(f"Exception: {e}")
+            try:
+                await CsmRestApi.check_for_unsupported_endpoint(request)
+            except DataAccessError as e:
+                Log.warn(f"Exception: {e}")
             resp = await handler(request)
             if isinstance(resp, DownloadFileEntity):
                 file_resp = web.FileResponse(resp.path_to_file)
