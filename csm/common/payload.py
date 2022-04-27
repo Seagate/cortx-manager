f17de35a26f4caeb015fefc72f2c75d2d84fc8df
Alexander Voronov <alexander.voronov@seagate.com>
2022-04-27 16:24:50 +0300

    CORTX-30825: fix Codacy issues in csm/common/payload.py
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>

--- csm/common/payload.py
+++ csm/common/payload.py
@@ -13,8 +13,11 @@
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
-import os, errno, sys
-import json, toml, yaml, tarfile
+import os
+import json
+import toml
+import yaml
+import tarfile
 import configparser
 from typing import List
 
@@ -29,7 +32,7 @@ class Doc:
         return str(self._source)
 
     def load(self):
-        ''' Loads data from file of given format '''
+        """Load data from file of given format."""
         if not os.path.exists(self._source):
             return {}
         try:
@@ -38,14 +41,15 @@ class Doc:
             raise Exception('Unable to read file %s. %s' % (self._source, e))
 
     def dump(self, data):
-        ''' Dump the anifest file to desired file or to the source '''
+        """Dump the anifest file to desired file or to the source."""
         dir_path = os.path.dirname(self._source)
         if len(dir_path) > 0 and not os.path.exists(dir_path):
             os.makedirs(dir_path)
         self._dump(data)
 
+
 class Toml(Doc):
-    ''' Represents a TOML doc '''
+    """TOML document representation."""
 
     def __init__(self, file_path):
         Doc.__init__(self, file_path)
@@ -58,8 +62,9 @@ class Toml(Doc):
         with open(self._source, 'w') as f:
             toml.dump(data, f)
 
+
 class Json(Doc):
-    ''' Represents a JSON doc '''
+    """JSON document representation."""
 
     def __init__(self, file_path):
         Doc.__init__(self, file_path)
@@ -72,8 +77,9 @@ class Json(Doc):
         with open(self._source, 'w') as f:
             json.dump(data, f, indent=2)
 
+
 class Yaml(Doc):
-    ''' Represents a YAML doc '''
+    """YAML document representation."""
 
     def __init__(self, file_path):
         Doc.__init__(self, file_path)
@@ -86,25 +92,28 @@ class Yaml(Doc):
         with open(self._source, 'w') as f:
             yaml.dump(data, f)
 
+
 class Tar(Doc):
-    """Represents Tar File"""
+    """Tar file representation."""
 
     def __init__(self, file_path):
         Doc.__init__(self, file_path)
 
     def _dump(self, files: List):
         """
-        will create a tar file at source path.
+        Create a tar file at source path.
+
         :param files: Files and Directories to be Included in Tar File.
         :return: None
         """
         with tarfile.open(self._source, "w:gz") as tar:
             for each_file in files:
                 tar.add(each_file, arcname=os.path.basename(each_file),
-                    recursive=True)
+                        recursive=True)
+
 
 class Ini(Doc):
-    ''' Represents a YAML doc '''
+    """Ini document representation."""
 
     def __init__(self, file_path):
         self._config = configparser.ConfigParser()
@@ -119,8 +128,9 @@ class Ini(Doc):
         with open(self._source, 'w') as f:
             data.write(f)
 
+
 class Dict(Doc):
-    '''Represents Dictionary Without file'''
+    """Python dictionary representation as a document."""
 
     def __init__(self, data={}):
         Doc.__init__(self, data)
@@ -131,48 +141,57 @@ class Dict(Doc):
     def dump(self, data):
         self._source = data
 
-class Text(Doc):
 
-    '''Represents a TEXT doc'''
+class Text(Doc):
+    """Text document representation."""
+
     def __init__(self, file_path):
         Doc.__init__(self, file_path)
 
     def _load(self):
-        '''Loads data from text file'''
+        """Load data from text file."""
         with open(self._source, 'r') as f:
             return f.read()
 
     def _dump(self, data):
-        '''Dump the data to desired file or to the source'''
+        """Dump the data to desired file or to the source."""
         with open(self._source, 'w') as f:
             f.write(data)
 
+
 class JsonMessage(Json):
+    """Json respresentation as a document."""
+
     def __init__(self, json_str):
         """
-        Represents the Json Without FIle
-        :param json_str: Json String to be processed :type: str
+        Represent the Json Without FIle.
+
+        :param json_str: Json String to be processed.
+        :type: str.
         """
         Json.__init__(self, json_str)
 
     def load(self):
         """
-        Load the json to python interpretable Dictionary Object
-        :return: :type: Dict
+        Load the json to python interpretable Dictionary Object.
+
+        :return: :type: Dict.
         """
         return json.loads(self._source)
 
     def dump(self, data):
         """
-        Set's the data _source after converting to json
+        Set the data _source after converting to json.
+
         :param data: :type: Dict
         :return:
         """
         self._source = json.dumps(data)
         return self._source
 
+
 class Payload:
-    ''' implements a Payload in specified format. '''
+    """Payload in the specified format."""
 
     def __init__(self, doc):
         self._dirty = False
@@ -186,14 +205,15 @@ class Payload:
         return self._data
 
     def dump(self):
-        ''' Dump the anifest file to desired file or to the source '''
+        """Dump the anifest file to desired file or to the source."""
         self._doc.dump(self._data)
         self._dirty = False
 
     def _get(self, key, data):
-        ''' Obtain value for the given key '''
+        """Obtain value for the given key."""
         k = key.split('.', 1)
-        if k[0] not in data.keys(): return None
+        if k[0] not in data.keys():
+            return None
         return self._get(k[1], data[k[0]]) if len(k) > 1 else data[k[0]]
 
     def get(self, key):
@@ -206,12 +226,12 @@ class Payload:
         if len(k) == 1:
             data[k[0]] = val
             return
-        if k[0] not in data.keys() or type(data[k[0]]) != self._doc._type:
+        if k[0] not in data.keys() or not isinstance(data[k[0]], self._doc._type):
             data[k[0]] = {}
         self._set(k[1], val, data[k[0]])
 
     def set(self, key, val):
-        ''' Sets the value into the DB for the given key '''
+        """Set the value into the DB for the given key."""
         self._set(key, val, self._data)
         self._dirty = True
 
@@ -228,27 +248,28 @@ class Payload:
         self._dirty = dirty
         return value
 
-    def convert(self, map, payload):
+    def convert(self, schema, payload):
         """
-        Converts 1 Schema to 2nd Schema depending on mapping dictionary.
-        :param map: mapping dictionary :type:Dict
+        Convert fist Schema to 2nd Schema depending on mapping dictionary.
+
+        :param schema: mapping dictionary :type:Dict
         :param payload: Payload Class Object with desired Source.
         :return: :type: Dict
-        Mapping file example - 
+        Mapping file example -
         key <input schema> : value <output schema>
         """
-        for key in map.keys():
+        for key in schema.keys():
             val = self.get(key)
-            payload.set(map[key], val)
+            payload.set(schema[key], val)
         return payload
 
     def data(self):
         return self._data
 
+
 class CommonPayload:
-    """
-    Implements a common payload to represent Json, Toml, Yaml, Ini Doc.
-    """
+    """Common payload representing Json, Toml, Yaml, Ini Doc."""
+
     def __init__(self, source):
         self._source = source
         # Mapping of file extensions and doc classes.
@@ -259,7 +280,7 @@ class CommonPayload:
         self._doc = self.get_doc_type()
 
     def get_doc_type(self):
-        '''Get mapped doc class object bases on file extension'''
+        """Get mapped doc class object bases on file extension."""
         try:
             extension = os.path.splitext(self._source)[1][1:].strip().lower()
             doc_obj = self._MAP[extension]
@@ -268,12 +289,11 @@ class CommonPayload:
             raise KeyError(f"Unsupported file type:{error}")
         except Exception as e:
             raise Exception(f"Unable to read file {self._source}. {e}")
-        
+
     def load(self):
-        ''' Loads data from file of given format'''
+        """Load data from file of given format."""
         return self._doc._load()
 
     def dump(self, data):
-        ''' Dump the data to desired file or to the source '''
+        """Dump the data to desired file or to the source."""
         self._doc._dump(data)
-    
