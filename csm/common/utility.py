2065c977647b0272090fe1e219520b549e7c7ee4
vaishnavirjoshi <vaishnavi.r.joshi@seagate.com>
2022-09-05 20:54:03 +0530

    added memory limit conversion function
    Signed-off-by: vaishnavirjoshi <vaishnavi.r.joshi@seagate.com>

--- csm/common/utility.py
+++ csm/common/utility.py
@@ -126,8 +126,8 @@ class Utility:
                 Log.error(f"Unable to fetch the configurations from consul: {e}")
                 raise e
                 
-     @staticmethod
-     def conversion_mem_limit(intVal, convertFrom, convertTo):
+    @staticmethod
+    def conversion_mem_limit(intVal, convertFrom, convertTo):
         convertToByte = {'B' : 1, 'K' : 1000, 'M' : 1000**2, 'G' : 1000**3, 'T' : 1000 ** 4, 'P' : 1000 ** 5,'Ki' : 1024, 'Mi' : 1024**2, 'Gi' : 1024**3, 'Ti' : 1024 ** 4, 'Pi' : 1024 ** 5}
         valInByte = convertToByte.get(convertFrom) * intVal
         val = convertToByte.get(convertTo)
